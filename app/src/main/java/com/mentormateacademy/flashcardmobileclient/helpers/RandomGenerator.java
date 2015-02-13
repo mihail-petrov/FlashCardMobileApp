9abac9df28b7fb6980fd24764395ff6c9eb3ab5b
Mihail Petrov <petrovm@mail.bg>
2015-02-13 22:01:54 +0200

    App commit: Add random generator helper

---
+++ app/src/main/java/com/mentormateacademy/flashcardmobileclient/helpers/RandomGenerator.java
@@ -0,0 +1,69 @@
+package com.mentormateacademy.flashcardmobileclient.helpers;
+
+import java.util.Random;
+
+/**
+ * Created by p_m_t_000 on 2/13/2015.
+ */
+public class RandomGenerator {
+
+    private static final String alphabetCollection = "abcdefghijklmnopqrstuvwxyz";
+    private Random randomGenerator;
+
+    //
+    // ===============================================
+
+    public RandomGenerator() {
+        this.randomGenerator = new Random();
+    }
+
+
+    //
+    // ===============================================
+    public String generateString(int maxLength, boolean addSpace) {
+        return this.generateString(0, maxLength, addSpace);
+    }
+
+    /**
+     * @param minLength
+     * @param maxLength
+     * @return
+     */
+    public String generateString(int minLength, int maxLength, boolean addSpace) {
+
+        StringBuilder stringAccumulator = new StringBuilder();
+
+        for (int i = minLength; i < maxLength; i++) {
+
+
+            int position = this.generateInteger(alphabetCollection.length());
+            char element = alphabetCollection.charAt(position);
+
+            if (addSpace) {
+                if (i % 5 == 0) stringAccumulator.append(" ");
+            }
+            stringAccumulator.append(element);
+        }
+
+        return stringAccumulator.toString();
+    }
+
+    /**
+     * @param maxLength
+     * @return
+     */
+    public int generateInteger(int maxLength) {
+        return this.randomGenerator.nextInt(maxLength);
+    }
+
+    /**
+     * @param minLength
+     * @param maxLength
+     * @return
+     */
+    public int generateInteger(int minLength, int maxLength) {
+
+        int randomNumberElement = this.randomGenerator.nextInt(maxLength);
+        return (minLength + randomNumberElement);
+    }
+}
