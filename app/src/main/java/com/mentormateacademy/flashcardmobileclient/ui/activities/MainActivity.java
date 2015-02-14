e8600e63af885c0f66f9d8a818f5d056cd1870f9
Nikolay Ivanov <nikolay.iv.ivanov@abv.bg>
2015-02-14 22:17:55 +0200

    Bugs in MainActivity's layout fixed

--- app/src/main/java/com/mentormateacademy/flashcardmobileclient/ui/activities/MainActivity.java
+++ app/src/main/java/com/mentormateacademy/flashcardmobileclient/ui/activities/MainActivity.java
@@ -6,25 +6,67 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewTreeObserver;
+import android.widget.LinearLayout;
 
 import com.mentormateacademy.flashcardmobileclient.R;
 
 public class MainActivity extends ActionBarActivity {
+
+    private static boolean addFragmentOnce = false;
+    private LinearLayout linearLayoutMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setObserverOnMainLayout();
 
+        if(!addFragmentOnce) {
+            addInitialFragment();
+            addFragmentOnce =true;
+        }
+
+
+    }
+
+    private void setObserverOnMainLayout() {
+        linearLayoutMain = (LinearLayout) findViewById(R.id.mainLinearLayout);
+        final ViewTreeObserver treeObserver = linearLayoutMain.getViewTreeObserver();
+
+        final ViewTreeObserver.OnGlobalLayoutListener layoutListener = new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                int currentApiVersion = android.os.Build.VERSION.SDK_INT;
+                if (currentApiVersion >= 16) {
+                    linearLayoutMain.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                } else {
+                    linearLayoutMain.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                }
+
+                //Check if a there is any fragment on the screen
+                try {
+                    LogInFragment logInFragment = (LogInFragment) getSupportFragmentManager().findFragmentById(R.id.container);
+                    SignUpFragment signUpFragment = (SignUpFragment) getSupportFragmentManager().findFragmentById(R.id.container);
+                    if(logInFragment == null && signUpFragment == null){
+                        addInitialFragment();
+                    }
+                }catch (ClassCastException e){
+                    e.printStackTrace();
+                }
+            }
+        };
+        treeObserver.addOnGlobalLayoutListener(layoutListener);
+    }
+
+    private void addInitialFragment() {
         LogInFragment logInFragment = new LogInFragment();
         FragmentManager manager = getSupportFragmentManager();
         FragmentTransaction ft = manager.beginTransaction();
         ft.add(R.id.container, logInFragment);
         ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
         ft.commit();
-
-
     }
 
 
