86a74729b6481458eb307e4978e3ead27aec9843
Mihail Petrov <petrovm@mail.bg>
2015-02-15 21:31:16 +0200

    Layout commit: Add layout for Deck list

---
+++ app/src/main/java/com/mentormateacademy/flashcardmobileclient/ui/fragments/lists/CardListFragment.java
@@ -0,0 +1,7 @@
+package com.mentormateacademy.flashcardmobileclient.ui.fragments.lists;
+
+/**
+ * Created by p_m_t_000 on 2/15/2015.
+ */
+public class CardListFragment {
+}
--- app/src/main/java/com/mentormateacademy/flashcardmobileclient/ui/fragments/lists/DecksListFragment.java
+++ app/src/main/java/com/mentormateacademy/flashcardmobileclient/ui/fragments/lists/DecksListFragment.java
@@ -1,6 +1,23 @@
 package com.mentormateacademy.flashcardmobileclient.ui.fragments.lists;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.mentormateacademy.flashcardmobileclient.R;
 
 public class DecksListFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View fragmentView = inflater.inflate(R.layout.fragment_list_deck, container, false);
+        ListView deckListView = (ListView) fragmentView.findViewById(R.id.newCardContentEditText);
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
