5aa7c7d3901a0fd89a80441db7b7d9edfcf63fa2
Nikolay Ivanov <nikolay.iv.ivanov@abv.bg>
2015-02-17 11:20:23 +0200

    Improvements in MainActivity's layout

--- app/src/main/java/com/mentormateacademy/flashcardmobileclient/ui/activities/LogInFragment.java
+++ app/src/main/java/com/mentormateacademy/flashcardmobileclient/ui/activities/LogInFragment.java
@@ -1,6 +1,7 @@
 package com.mentormateacademy.flashcardmobileclient.ui.activities;
 
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
@@ -13,6 +14,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -24,8 +26,6 @@ public class LogInFragment extends Fragment implements View.OnClickListener{
 
     private EditText editTextUsername, editTextPassword;
     private Button buttonLogIn, buttonForgotPassword, buttonSignUp;
-    private LinearLayout linearLayoutLogIn;
-    private RelativeLayout relativeLayoutLogIn;
 
 
     @Override
@@ -39,85 +39,6 @@ public class LogInFragment extends Fragment implements View.OnClickListener{
 
         initializeViewsAndSetListeners();
 
-        setObserverForLayout();
-
-    }
-
-
-    private void setObserverForLayout() {
-        final ViewTreeObserver treeObserver = linearLayoutLogIn.getViewTreeObserver();
-
-        final ViewTreeObserver.OnGlobalLayoutListener layoutListener = new ViewTreeObserver.OnGlobalLayoutListener() {
-            @Override
-            public void onGlobalLayout() {
-                int currentApiVersion = android.os.Build.VERSION.SDK_INT;
-                if (currentApiVersion >= 16) {
-                    linearLayoutLogIn.getViewTreeObserver().removeOnGlobalLayoutListener(this);
-                } else {
-                    linearLayoutLogIn.getViewTreeObserver().removeGlobalOnLayoutListener(this);
-                }
-                DisplayMetrics displayMetrics = new DisplayMetrics();
-                getActivity().getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-                int width=displayMetrics.widthPixels;
-                int height=displayMetrics.heightPixels;
-                int dens=displayMetrics.densityDpi;
-                double wi=(double)width/(double)dens;
-                double he=(double)height/(double)dens;
-                double x = Math.pow(wi,2);
-                double y = Math.pow(he, 2);
-                double diagonalInches = Math.sqrt(x+y);
-
-                final double coefficient = diagonalInches / MainActivity.BASE_INCHES;
-
-
-                if(getActivity().getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
-
-                    adjustViews(coefficient, displayMetrics.density);
-
-                    buttonSignUp.setWidth((int) (buttonForgotPassword.getWidth() * coefficient));
-                    buttonForgotPassword.setWidth((int) (buttonForgotPassword.getWidth() * coefficient));
-                }else {
-
-                    adjustViews(coefficient, displayMetrics.density);
-
-                    buttonSignUp.setWidth((int) (buttonForgotPassword.getWidth() * ((float) width / (float) height) * coefficient));
-                    buttonForgotPassword.setWidth((int) (buttonForgotPassword.getWidth() * ((float) width / (float) height) * coefficient));
-
-                }
-
-            }
-        };
-
-        treeObserver.addOnGlobalLayoutListener(layoutListener);
-
-    }
-
-    //Adjust Views based on the screen size
-    private void adjustViews(double coefficient, float density) {
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT);
-
-        float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 14, getResources().getDisplayMetrics());
-
-        //params = (LinearLayout.LayoutParams) editTextUsername.getLayoutParams();
-        params.setMargins(0, (int) (50*coefficient*density),0,0);
-        //editTextUsername.setLayoutParams(params);
-        editTextUsername.setTextSize(TypedValue.COMPLEX_UNIT_PX,(float) (editTextUsername.getTextSize()*coefficient));
-
-        params.setMargins(0, (int) (50*coefficient*density),0,0);
-        //editTextPassword.setLayoutParams(params);
-        editTextPassword.setTextSize(TypedValue.COMPLEX_UNIT_PX,(float) (editTextPassword.getTextSize()*coefficient));
-
-        params.setMargins((int) (15 * coefficient), (int) (75 * coefficient),
-                (int) (15 * coefficient), (int) (25 * coefficient));
-        //buttonLogIn.setLayoutParams(params);
-        buttonLogIn.setTextSize(TypedValue.COMPLEX_UNIT_PX, (float) (buttonLogIn.getTextSize() * coefficient));
-
-        params.setMargins((int) (15 * coefficient), 0, (int) (15 * coefficient), 0);
-        //relativeLayoutLogIn.setLayoutParams(params);
-
-        buttonSignUp.setTextSize(TypedValue.COMPLEX_UNIT_PX, (float) (buttonSignUp.getTextSize() * coefficient));
-        buttonForgotPassword.setTextSize(TypedValue.COMPLEX_UNIT_PX, (float) (buttonForgotPassword.getTextSize() * coefficient));
     }
 
     private void initializeViewsAndSetListeners() {
@@ -129,9 +50,6 @@ public class LogInFragment extends Fragment implements View.OnClickListener{
         buttonSignUp.setOnClickListener(this);
         buttonForgotPassword.setOnClickListener(this);
 
-        linearLayoutLogIn = (LinearLayout) getActivity().findViewById(R.id.logInFragmentLayout);
-        relativeLayoutLogIn = (RelativeLayout) getActivity().findViewById(R.id.logInFragmentRelativeLayout);
-
         editTextUsername = (EditText) getActivity().findViewById(R.id.usernameLogInEditText);
         editTextPassword = (EditText) getActivity().findViewById(R.id.passwordLogInEditText);
 
@@ -140,6 +58,7 @@ public class LogInFragment extends Fragment implements View.OnClickListener{
 
     @Override
     public void onClick(View v) {
+        hideKeyboard();
         switch (v.getId()){
             case R.id.logInButton:
                 Intent newIntent = new Intent(getActivity(), ManageFlashCardsActivity.class);
@@ -157,6 +76,14 @@ public class LogInFragment extends Fragment implements View.OnClickListener{
             case R.id.forgotPasswordButton:
 
                 break;
+        }
+    }
+    private void hideKeyboard() {
+        // Check if no view has focus:
+        View view = getActivity().getCurrentFocus();
+        if (view != null) {
+            InputMethodManager inputManager = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputManager.hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
         }
     }
 }
--- app/src/main/java/com/mentormateacademy/flashcardmobileclient/ui/activities/SignUpFragment.java
+++ app/src/main/java/com/mentormateacademy/flashcardmobileclient/ui/activities/SignUpFragment.java
@@ -1,6 +1,7 @@
 package com.mentormateacademy.flashcardmobileclient.ui.activities;
 
 
+import android.content.Context;
 import android.content.res.Configuration;
 import android.os.Build;
 import android.os.Bundle;
@@ -12,6 +13,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -22,7 +24,6 @@ public class SignUpFragment extends Fragment implements View.OnClickListener{
 
     private EditText editTextUsername, editTextPassword, editTextConfirmPassword;
     private Button buttonLogIn, buttonSignUp;
-    private LinearLayout linearLayoutSignUp;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -34,7 +35,6 @@ public class SignUpFragment extends Fragment implements View.OnClickListener{
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         initializeViews();
-        setObserverForLayout();
     }
 
     private void initializeViews() {
@@ -48,76 +48,11 @@ public class SignUpFragment extends Fragment implements View.OnClickListener{
         editTextPassword = (EditText) getActivity().findViewById(R.id.passwordSignUpEditText);
         editTextConfirmPassword = (EditText) getActivity().findViewById(R.id.confirmPasswordSignUpEditText);
 
-        linearLayoutSignUp = (LinearLayout) getActivity().findViewById(R.id.signUpLinearLayout);
-    }
-
-    private void setObserverForLayout() {
-
-        final ViewTreeObserver treeObserver = linearLayoutSignUp.getViewTreeObserver();
-
-        final ViewTreeObserver.OnGlobalLayoutListener layoutListener = new ViewTreeObserver.OnGlobalLayoutListener() {
-            @Override
-            public void onGlobalLayout() {
-                int currentApiVersion = Build.VERSION.SDK_INT;
-                if (currentApiVersion >= 16) {
-                    linearLayoutSignUp.getViewTreeObserver().removeOnGlobalLayoutListener(this);
-                } else {
-                    linearLayoutSignUp.getViewTreeObserver().removeGlobalOnLayoutListener(this);
-                }
-                DisplayMetrics displayMetrics = new DisplayMetrics();
-                getActivity().getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-                int width=displayMetrics.widthPixels;
-                int height=displayMetrics.heightPixels;
-                int dens=displayMetrics.densityDpi;
-                double wi=(double)width/(double)dens;
-                double he=(double)height/(double)dens;
-                double x = Math.pow(wi,2);
-                double y = Math.pow(he, 2);
-                double diagonalInches = Math.sqrt(x+y);
-
-                final double coefficient = diagonalInches / MainActivity.BASE_INCHES;
-                adjustViews(coefficient, displayMetrics.density);
-            }
-        };
-
-        treeObserver.addOnGlobalLayoutListener(layoutListener);
-
-    }
-
-    //Adjust Views based on the screen size
-    private void adjustViews(double coefficient, float density) {
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT);
-
-        //float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 14, getResources().getDisplayMetrics());
-
-        //params = (LinearLayout.LayoutParams) editTextUsername.getLayoutParams();
-        params.setMargins(0, (int) (30*coefficient*density),0,0);
-        //editTextUsername.setLayoutParams(params);
-        editTextUsername.setTextSize(TypedValue.COMPLEX_UNIT_PX,(float) (editTextUsername.getTextSize()*coefficient));
-
-        params.setMargins(0, (int) (30*coefficient*density),0,0);
-        //editTextPassword.setLayoutParams(params);
-        editTextPassword.setTextSize(TypedValue.COMPLEX_UNIT_PX,(float) (editTextPassword.getTextSize()*coefficient));
-
-        params.setMargins(0, (int) (30*coefficient*density),0,0);
-        //editTextPassword.setLayoutParams(params);
-        editTextConfirmPassword.setTextSize(TypedValue.COMPLEX_UNIT_PX,(float) (editTextConfirmPassword.getTextSize()*coefficient));
-
-        params.setMargins((int) (15 * coefficient), (int) (50 * coefficient), (int) (15 * coefficient), 0);
-        //relativeLayoutLogIn.setLayoutParams(params);
-        buttonSignUp.setTextSize(TypedValue.COMPLEX_UNIT_PX, (float) (buttonSignUp.getTextSize() * coefficient));
-
-        params.setMargins((int) (15 * coefficient), (int) (25 * coefficient),
-                (int) (15 * coefficient), 0);
-        //buttonLogIn.setLayoutParams(params);
-        buttonLogIn.setTextSize(TypedValue.COMPLEX_UNIT_PX, (float) (buttonLogIn.getTextSize() * coefficient));
-
-
     }
 
     @Override
     public void onClick(View v) {
+        hideKeyboard();
         switch (v.getId()){
             case R.id.signUpButton:
 
@@ -127,5 +62,13 @@ public class SignUpFragment extends Fragment implements View.OnClickListener{
                 break;
         }
 
+    }
+    private void hideKeyboard() {
+        // Check if no view has focus:
+        View view = getActivity().getCurrentFocus();
+        if (view != null) {
+            InputMethodManager inputManager = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputManager.hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        }
     }
 }
